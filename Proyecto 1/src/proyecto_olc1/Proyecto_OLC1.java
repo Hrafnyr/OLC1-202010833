1c06e2c3ec4c9637eceaa6b895b7975552d869b3
Moises David Maldonado de León <moiseszurdomaldonado@gmail.com>
2022-08-17 16:09:59 -0600

    Creacion de interfaz

---
+++ Proyecto 1/src/proyecto_olc1/Proyecto_OLC1.java
@@ -0,0 +1,12 @@
+
+package proyecto_olc1;
+
+// Author: Moises David Maldonado de León - 202010833
+
+public class Proyecto_OLC1 {
+
+    public static void main(String[] args) {
+       //Inicio de interfaz 
+        interfaz v = new interfaz();
+    }
+}
