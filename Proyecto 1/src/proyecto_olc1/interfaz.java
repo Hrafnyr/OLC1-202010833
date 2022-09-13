b7e007a2469b1b34700a4c8075e4012f5153194c
Moises David Maldonado de León <moiseszurdomaldonado@gmail.com>
2022-09-13 01:33:19 -0600

    Traduccion Go 75%, gramatica 95%

--- Proyecto 1/src/proyecto_olc1/interfaz.java
+++ Proyecto 1/src/proyecto_olc1/interfaz.java
@@ -8,6 +8,7 @@ package proyecto_olc1;
 import Analizadores.A_sintactico;
 import Analizadores.Analizador_Lexico;
 import Analizadores.claseErrores;
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -23,9 +24,16 @@ import java.io.StringReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java_cup.runtime.Symbol;
+import static javafx.scene.paint.Color.color;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 
 /**
  *
@@ -39,7 +47,7 @@ public class interfaz extends javax.swing.JFrame {
     FileInputStream entrada;
     FileOutputStream salida;
     String nombre;
-    
+    int contV = 0;
     
 
     
@@ -57,7 +65,92 @@ public class interfaz extends javax.swing.JFrame {
         this.jButton3.setOpaque(true);
         this.jButton4.setOpaque(true);
         this.txtErrores.setEditable(false);
+        colors();
         
+    }
+    
+    
+    //METODO PARA ENCONTRAR LAS ULTIMAS CADENAS
+    private int findLastNonWordChar(String text, int index) {
+        while (--index >= 0) {
+            //  \\W = [A-Za-Z0-9]
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+        }
+        return index;
+    }
+
+    //METODO PARA ENCONTRAR LAS PRIMERAS CADENAS 
+    private int findFirstNonWordChar(String text, int index) {
+        while (index < text.length()) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+            index++;
+        }
+        return index;
+    }
+
+    //METODO PARA PINTAS LAS PALABRAS RESEVADAS
+    private void colors() {
+
+        final StyleContext cont = StyleContext.getDefaultStyleContext();
+
+        //COLORES 
+        final AttributeSet attred = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, new Color(255, 0, 35));
+        final AttributeSet attgreen = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, new Color(34,139,34));
+        final AttributeSet attblue = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, new Color(0, 147, 255));
+        final AttributeSet attblack = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, new Color(0, 0, 0));
+
+        //STYLO 
+        DefaultStyledDocument doc = new DefaultStyledDocument() {
+            public void insertString(int offset, String str, AttributeSet a) throws BadLocationException {
+                super.insertString(offset, str, a);
+
+                String text = getText(0, getLength());
+                int before = findLastNonWordChar(text, offset);
+                if (before < 0) {
+                    before = 0;
+                }
+                int after = findFirstNonWordChar(text, offset + str.length());
+                int wordL = before;
+                int wordR = before;
+
+                while (wordR <= after) {
+                    if (wordR == after || String.valueOf(text.charAt(wordR)).matches("\\W")) {
+                        if (text.substring(wordL, wordR).matches("(\\W)*(si|fin_si|ingresar|como|con_valor|de_lo_contrario|o_si|entonces|repetir|hasta_que|ejecutar)")) {
+                            setCharacterAttributes(wordL, wordR - wordL, attblue, false);
+                        } else if (text.substring(wordL, wordR).matches("(\\W)*(numero|cadena|bolean|caracter|para|hasta|fin_para|con_incremental|retornar|con_parametros|imprimir|imprimir_nl)")) {
+                            setCharacterAttributes(wordL, wordR - wordL, attgreen, false);
+                        } else if (text.substring(wordL, wordR).matches("(\\W)*(inicio|fin|segun|hacer|fin_segun|mientras|fin_mientras|metodo|fin_metodo|funcion|fin_funcion)")) {
+                            setCharacterAttributes(wordL, wordR - wordL, attred, false);
+                        } else {
+                            setCharacterAttributes(wordL, wordR - wordL, attblack, false);
+                        }
+                        wordL = wordR;
+
+                    }
+                    wordR++;
+                }
+            }
+
+            public void romeve(int offs, int len) throws BadLocationException {
+                super.remove(offs, len);
+
+                String text = getText(0, getLength());
+                int before = findLastNonWordChar(text, offs);
+                if (before < 0) {
+                    before = 0;
+                }
+            }
+        };
+
+        JTextPane txt = new JTextPane(doc);
+        String temp = txtCodigo.getText();
+        txtCodigo.setStyledDocument(txt.getStyledDocument());
+        txtCodigo.setText(temp);
+
     }
     
     //abrir archivo
@@ -90,14 +183,14 @@ public class interfaz extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        txtCodigo = new javax.swing.JEditorPane();
         btRun = new javax.swing.JButton();
         btClean = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         txtErrores = new javax.swing.JTextField();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        txtCodigo = new javax.swing.JTextPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -105,6 +198,7 @@ public class interfaz extends javax.swing.JFrame {
         jMenu3 = new javax.swing.JMenu();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenuItem6 = new javax.swing.JMenuItem();
@@ -119,13 +213,6 @@ public class interfaz extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
         jLabel1.setText("OLC1_2S_2022 202010833");
-
-        txtCodigo.setFont(new java.awt.Font("Consolas", 0, 18)); // NOI18N
-        txtCodigo.setToolTipText("");
-        txtCodigo.setCaretColor(new java.awt.Color(0, 102, 102));
-        txtCodigo.setSelectedTextColor(new java.awt.Color(255, 255, 237));
-        txtCodigo.setSelectionColor(new java.awt.Color(0, 102, 102));
-        jScrollPane1.setViewportView(txtCodigo);
 
         btRun.setBackground(new java.awt.Color(51, 153, 0));
         btRun.setFont(new java.awt.Font("DialogInput", 1, 24)); // NOI18N
@@ -179,12 +266,17 @@ public class interfaz extends javax.swing.JFrame {
             }
         });
 
+        txtCodigo.setFont(new java.awt.Font("Consolas", 0, 24)); // NOI18N
+        txtCodigo.setSelectedTextColor(new java.awt.Color(255, 255, 204));
+        txtCodigo.setSelectionColor(new java.awt.Color(0, 102, 102));
+        jScrollPane2.setViewportView(txtCodigo);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(54, Short.MAX_VALUE)
+                .addGap(54, 54, 54)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel1)
@@ -193,33 +285,31 @@ public class interfaz extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btRun, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1321, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txtErrores, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 684, Short.MAX_VALUE)
                         .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(24, 24, 24)
-                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING))
                 .addGap(49, 49, 49))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(47, 47, 47)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(btRun, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btClean, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(9, 9, 9)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 607, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(btClean, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(53, 53, 53)))
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 587, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(txtErrores, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -270,6 +360,14 @@ public class interfaz extends javax.swing.JFrame {
         });
         jMenu3.add(jMenuItem4);
 
+        jMenuItem7.setText("Árbol Sintáctico");
+        jMenuItem7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem7ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem7);
+
         jMenuBar1.add(jMenu3);
 
         jMenu4.setText("Ver");
@@ -315,38 +413,45 @@ public class interfaz extends javax.swing.JFrame {
     }//GEN-LAST:event_txtErroresActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        String txt = A_sintactico.txtPython;
         
-        //filtro
-        FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos py", ".py","py");
-        ver.setFileFilter(filtro);
-        ver.setMultiSelectionEnabled(true);
-        
-        //Metodo para guardar
-        if(ver.showDialog(null, "Guardar")==JFileChooser.APPROVE_OPTION){
-            archivo = ver.getSelectedFile().getAbsoluteFile() ;
-            if (archivo != null) {
-            String nombre = ver.getSelectedFile().getName();
-                try {
-                    FileWriter data = new FileWriter(archivo+".py");
-                    data.write(txt);
-                    data.close();
-                } catch (Exception e) {
-                    
-                }
-            }
+        if(contV==0){
+               JOptionPane.showMessageDialog(null, "Debe presionar el botón Run ");
+            
         }else{
-                return;
-            }
+            String txt = A_sintactico.txtPython;
+        
+            //filtro
+            FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos py", ".py","py");
+            ver.setFileFilter(filtro);
+            ver.setMultiSelectionEnabled(true);
+
+            //Metodo para guardar
+            if(ver.showDialog(null, "Guardar")==JFileChooser.APPROVE_OPTION){
+                archivo = ver.getSelectedFile().getAbsoluteFile() ;
+                if (archivo != null) {
+                String nombre = ver.getSelectedFile().getName();
+                    try {
+                        FileWriter data = new FileWriter(archivo+".py");
+                        data.write(txt);
+                        data.close();
+                    } catch (Exception e) {
+
+                    }
+                }
+            }else{
+                    return;
+                }
+        }
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void btRunActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btRunActionPerformed
-   
+        contV=1;
         //Limpiar errores para nueva verificacion y tabla
         Analizador_Lexico.TError.clear();
         
         //limpiar textoGuardado
         A_sintactico.txtPython="";
+        A_sintactico.txtGo="";
         
         //mostrar errores
         txtErrores.setEditable(true);
@@ -398,6 +503,7 @@ public class interfaz extends javax.swing.JFrame {
     }//GEN-LAST:event_btRunActionPerformed
 
     private void btCleanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btCleanActionPerformed
+       contV=0;
        txtCodigo.setText("");
        txtErrores.setEditable(true);
        txtErrores.setText("0");
@@ -439,6 +545,10 @@ public class interfaz extends javax.swing.JFrame {
     private void jMenu3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jMenu3ActionPerformed
+
+    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem7ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -492,9 +602,10 @@ public class interfaz extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JEditorPane txtCodigo;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextPane txtCodigo;
     private javax.swing.JTextField txtErrores;
     // End of variables declaration//GEN-END:variables
 }
